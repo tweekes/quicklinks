2554024609941953815249741daf636c806e4ac7
tweekes <tom.weekes@gmail.com>
2015-09-03 17:42:41 +0100

    search stuff

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,122 +1,18 @@
 angular.module('app').controller(
     "SearchModalController",
     function( $scope, modals ) {
-        $scope.searchResults = [
-          {
-            sectionTitle:"Life Claims Replacement",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "QPS Manage Custom Locations",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "see BRL3394 and BRL3727 for details in Mail Code Generation in the below Documant"
-          },
-          {
-            sectionTitle:"Life Claims Replacement",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "QPS Manage Custom Locations",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Username: tom.weekes Password: 3CYg-M78VK;3"
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          },
-          {
-            sectionTitle:"EPay",
-            sectionKey:"lifeClaimsReplacement",
-            itemTitle: "Share Point ",
-            link: "http://sp.sunlifecorp.com/sites/QPSDR/QPSDocumentsRequirements/05.%20QBEV04-Sale/UC%2019.06%20Manage%20Custom%20Locations.doc",
-            note: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen "
-          }
-        ];
-
+        $scope.searchResults = [];
         $scope.pgResultItems = new Pager($scope.searchResults,8,4);
-
+        $scope.search = function (event) {
+          $scope.searchResults = [];
+          if (event.charCode == 13) {
+              console.log("Enter presed and search text is:" + $scope.searchText)
+          }
+        };
         $scope.close = modals.resolve;
         $scope.dismiss = modals.reject;
     }
 );
-
-var matchTerms = function(item,reTerms) {
-    var rank = 0;
-
-    // match against title
-    _.each(reTerms,function(re){
-        if (item.title.search(re) != -1) {
-            rank++;
-        }
-    });
-
-    // match against link
-    _.each(reTerms,function(re){
-        if (item.link.search(re) != -1) {
-            rank++;
-        }
-    });
-
-    // match against note
-    _.each(reTerms,function(re){
-        if (item.note.search(re) != -1) {
-            rank++;
-        }
-    });
-
-    return rank;
-
-    // console.log("term = " + terms + "item title = " + item.title);
-};
-
 
 // http://stackoverflow.com/questions/6961615/using-regexp-to-dynamically-create-a-regular-expression-and-filter-content
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
@@ -132,49 +28,76 @@ function SearchMgr(refSections) {
     });
 
     _.each(this.refSections, function(section) {
-        _.each(section.jumpItems, function(item) {
-            var rank = matchTerms(item,reTerms);
-            if (rank > 0) {
-                console.log("push");
-                results.push(
-                    {
-                        sectionTitle: section.title,
-                        sectionKey: section.key,
-                        itemTitle: item.title,
-                        link: item.link,
-                        note: item.note,
-                        rank: rank
-                    }
-                );
-            }
-        });
+        console.log("Processing: " + section.title);
+        searchSection(results,section,reTerms);
+        searchList(results,section,section.jumpItems,reTerms);
+        searchList(results,section,section.linkItems,reTerms);
     });
 
+    results = results.sort(function(a,b) {return b.rank - a.rank});
     return results;
   };
 
-
-  /*
-  section[]
-    -title
-    -comment
-    jumpItems[]
-      -title
-      -link
-      -note
-    linkItems[]
-      -title
-      -link
-      -note
-  */
-
-
-
-
-
-
-
-
-
-
 }
+
+var searchList = function(results,section,itemList,reTerms) {
+  _.each(itemList, function(item) {
+      var rank = matchTerms(item,reTerms);
+      if (rank > 0) {
+          results.push(
+              {
+                  sectionTitle: section.title,
+                  sectionKey: section.key,
+                  itemTitle: item.title,
+                  link: item.link,
+                  text: item.note,
+                  rank: rank
+              }
+          );
+      }
+  });
+};
+
+var searchSection = function(results,section,reTerms) {
+    var rank = 0;
+    _.each(reTerms,function(re){
+        if (section.title.search(re) != -1) {
+            rank++;
+        }
+        if (section.comment.search(re) != -1) {
+            rank++;
+        }
+    });
+    if (rank > 0) {
+      results.push(
+          {
+              sectionTitle: section.title,
+              sectionKey: section.key,
+              itemTitle: "",
+              link: "",
+              text: section.comment,
+              rank: rank
+          }
+      );
+    }
+};
+
+var matchTerms = function(item,reTerms) {
+    var rank = 0;
+    // match against title,
+    // search() returns the index of start of term if found, else -1.
+    _.each(reTerms,function(re){
+        if (item.title.search(re) != -1) {
+            rank++;
+        }
+        if (item.link.search(re) != -1) {
+            rank++;
+        }
+        if (item.note.search(re) != -1) {
+            rank++;
+        }
+    });
+
+    return rank;
+    // console.log("term = " + terms + "item title = " + item.title);
+};
