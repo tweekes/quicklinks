19c4c680a0d80e6c7c97ca358587aa056317d4fd
tweekes <tom.weekes@gmail.com>
2015-09-30 20:11:42 +0100

    Started on Url testing

--- routes/urls.js
+++ routes/urls.js
@@ -2,29 +2,18 @@ var express = require('express');
 var http = require("http");
 var router = express.Router();
 
+// http://localhost:3000/urls/validate?path=http://www.rte.ie/news/2015/0930/731284-syria/
 router.get('/validate', function(req, res, next) {
-    var urlText = req.query.urltext;
-    console.log("urltext " + urlText );
-
-    var hostName = "Xjimmurraymusic.com";  // http://www.jimmurraymusic.com/ddd-tuning/introduction.html
-    var pathStr = "http://www.jimmurraymusic.com/ddd-tuning/introduction.htmlX";
-    var options = {method: 'HEAD', host: hostName, port: 80, path: pathStr};
-    var outBoundReq = http.request(options, function(r) {
-            console.log(JSON.stringify(r.headers));
-            res.status(200);
-            res.send("ok");
-        });
-
-    outBoundReq.on('error', function(err) {
-        handlerError('problem with request: ' + err.message,err,res)
+    var paramPath = req.query.path;
+    http.get(paramPath, function (r) {
+        console.log("Got response: " + r.statusCode);
+        res.status(200);
+        res.send({"path":paramPath,"status":r.statusCode});
+    }).on('error', function (err) {
+        res.status(404); // NOt found!
+        res.send({"path":paramPath,"status":404,message:err.message,errno:err.errno, code: err.code});
     });
-    outBoundReq.end();
 });
 
-var handlerError = function(eMsg,err,res) {
-    console.log(eMsg + '\n' + err);
-    res.status(400);
-    res.send({'error':{message:eMsg}});
-};
 
 module.exports = router;
